88987ce00c39b9c04c44b0a2747f3b8b2bac4ac7
MateoRT007 <mateort520@gmail.com>
2023-10-20 23:14:09 -0500

    Se realizan los cambios a las dos clases solicitados

--- src/main/java/org/example/ejercicios/Facturaciom2.java
+++ src/main/java/org/example/ejercicios/Facturaciom2.java
@@ -1,31 +1,32 @@
 package org.example.ejercicios;
 
 import java.util.List;
+import java.time.LocalDate;
 
 public class Facturaciom2 {
-	record Factura(String cliente, String fechafactura, List<Double> precioProducto) {
+	record Factura(String cliente,  LocalDate fechafactura, List<Double> precioProducto) {
 	}
 
 	public static void main(String[] args) {
-		Factura factura1 = new Factura("Maria Sannabria", "Julio 1, 1999", List.of(4500D, 5000D, 2500D));
-		Factura factura2 = new Factura("Juan Perez", "Agosto 15, 2000", List.of(3500D, 6000D, 2800D));
+		Factura factura1 = new Factura("Maria Sannabria", LocalDate.of(1999, 7, 1), List.of(4500D, 5000D, 2500D));
+		Factura factura2 = new Factura("Juan Perez", LocalDate.of(2000, 8, 15), List.of(3500D, 6000D, 2800D));
 
-		double totalValorFact1 = 0;
-		for (Double precio : factura1.precioProducto) {
-			totalValorFact1 += precio;
-		}
 		System.out.println("Factura 1" );
 		System.out.println("Cliente: " + factura1.cliente());
-		System.out.println("Total: " + totalValorFact1);
+		double total1 = calcularTotalFacturas(factura1.precioProducto);
+		System.out.println("Total: " + total1);
 
-
-		double totalValorFact2 = 0;
-		for (Double precio : factura2.precioProducto) {
-			totalValorFact2 += precio;
-		}
 		System.out.println("Factura 2" );
 		System.out.println("Cliente: " + factura2.cliente());
-		System.out.println("Total: " + totalValorFact2);
+		double total2 = calcularTotalFacturas(factura2.precioProducto);
+		System.out.println("Total: " + total2);
 	}
 
+	public static double calcularTotalFacturas (List<Double> precios){
+		double total = 0;
+		for (Double precio : precios) {
+			total += precio;
+		}
+		return total;
+	}
 }
